50fda973134ed48c25076f1e201524d19b85b2af
Shobhit <imshobhit.sb@gmail.com>
2021-06-11 11:27:04 +0530

    Created views and forms

--- pdf/models.py
+++ pdf/models.py
@@ -11,3 +11,6 @@ class Profile(models.Model):
     university = models.CharField(max_length=200)
     previous_work = models.TextField(max_length=1000)
     skills =  models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email = request.POST.get("email","")
+        phone = request.POST.get("phone","")
+        summary = request.POST.get("summary","")
+        degree = request.POST.get("degree","")
+        school = request.POST.get("school","")
+        university = request.POST.get("university","")
+        previous_role = request.POST.get("previous_work","")
+        skills = request.POST.get("skills","")
+
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,university=university,previous_work=previous_role,skills=skills)
+        profile.save()
+
+    return render(request,'pdf/accept.html') 
